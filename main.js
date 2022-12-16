b93f3077cca0b2751166eaedb5015cdd3523fb42
artjmaria <artjmaria@gmail.com>
2022-12-15 23:39:39 -0600

    modal practice

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
 // Get the modal
+
 const modal = document.getElementById("myModal");
 const timedModal = document.getElementById("timed-modal");
 
 // Get the button that opens the modal
+
 const btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modals
+
 const span = document.getElementsByClassName("close")[0];
 const timedSpan = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
+
 span.onclick = function() {
   modal.style.display = "none";
 }
 
 // Close Timed Modal
+
 timedSpan.onclick = function() {
   timedModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
   if (event.target == modal || event.target == timedModal) {
     modal.style.display = "none";
